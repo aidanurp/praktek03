99aaf6361826aed077300b0d12ca468a200e5c93
Aidanurp <14maret1998@gmail.com>
2018-04-06 04:51:40 +0800

    buat method cetakinfo

--- src/praktek03/sepedamotor.java
+++ src/praktek03/sepedamotor.java
@@ -4,7 +4,12 @@ public class sepedamotor {
     String warna;
     String merk;
     String jenis_motor;
-    double harga;
+    double harga; 
     
-    
+    void cetakInfo(){
+        System.out.println("Warna \t: "+warna+"\n"+
+                "Merk \t: "+merk+"\n"+
+                "Jenis Motor \t: "+jenis_motor+"\n"+
+                "Harga \t "+harga);
+    }
 }
