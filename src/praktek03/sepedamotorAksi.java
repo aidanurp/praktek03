d3fcfcb911ba332e65be8a63e6af8efb56f0b78b
Aidanurp <14maret1998@gmail.com>
2018-04-06 05:07:42 +0800

    instance, buat atribut dan panggil method

--- src/praktek03/sepedamotorAksi.java
+++ src/praktek03/sepedamotorAksi.java
@@ -4,6 +4,12 @@ package praktek03;
 
 public class sepedamotorAksi {
     public static void main(String[] args) {
+        sepedamotor a = new sepedamotor();
+        a.warna="ungu";
+        a.merk="piagio";
+        a.jenis_motor="vespa";
+        a.harga=4000;
         
+        a.cetakInfo();
     }
 }
