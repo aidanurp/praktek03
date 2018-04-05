aeb68b4b5a2d750fcebef2cba8fe0b9a5409197c
Aidanurp <14maret1998@gmail.com>
2018-04-06 05:12:25 +0800

    buat getter dan shetter

--- src/praktek03/sepedamotor.java
+++ src/praktek03/sepedamotor.java
@@ -12,4 +12,38 @@ public class sepedamotor {
                 "Jenis Motor \t: "+jenis_motor+"\n"+
                 "Harga \t "+harga);
     }
+
+    public String getWarna() {
+        return warna;
+    }
+
+    public void setWarna(String warna) {
+        this.warna = warna;
+    }
+
+    public String getMerk() {
+        return merk;
+    }
+
+    public void setMerk(String merk) {
+        this.merk = merk;
+    }
+
+    public String getJenis_motor() {
+        return jenis_motor;
+    }
+
+    public void setJenis_motor(String jenis_motor) {
+        this.jenis_motor = jenis_motor;
+    }
+
+    public double getHarga() {
+        return harga;
+    }
+
+    public void setHarga(double harga) {
+        this.harga = harga;
+    }
+    
+    
 }
