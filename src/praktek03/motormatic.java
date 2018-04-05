47fbb733989af070142a591c7845e50ca9200738
Aidanurp <14maret1998@gmail.com>
2018-04-06 05:41:01 +0800

    atribut dan method

--- src/praktek03/motormatic.java
+++ src/praktek03/motormatic.java
@@ -2,5 +2,9 @@
 package praktek03;
 
 public class motormatic extends sepedamotor{
+    private String jenis_transmisi;
     
+    void throttle(){
+        System.out.println("gearbox manual");
+    }
 }
